1d138ee6a2f65e66a10987c7d038ed532e0be560
samdouble <samleafs@protonmail.com>
2023-11-03 22:10:09 -0400

    Connect to MongoDB database

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from abc import ABC
-import sys    
+import sys
+import lib.db
 
 def handler(event, context):
+    db.get_database()
     print("Hello World!")
     return 'Hello from AWS Lambda using Python' + sys.version + '!'
 
