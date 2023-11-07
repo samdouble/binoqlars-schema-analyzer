debfe07267dcfbb72c439a1bfcf82ad3307dc484
samdouble <samleafs@protonmail.com>
2023-11-06 22:25:14 -0500

    Get first connection from MongoDB

--- src/utils/secrets/MongoDbAtlasSecretsManager.py
+++ src/utils/secrets/MongoDbAtlasSecretsManager.py
@@ -1,3 +1,4 @@
+import json
 from src.utils.secrets.SecretsManager import SecretsManager
 
 class MongoDbAtlasSecretsManager(SecretsManager):
@@ -5,6 +6,6 @@ class MongoDbAtlasSecretsManager(SecretsManager):
     def get_connection_string(cls):
         secret_name = 'datajitsu_6c98210f-ff3b-4d4a-8935-698fe2ad0287'
         secret = cls.get_secret(secret_name)
-        print(secret)
-        connection_string = secret['CONNECTION_STRING']
+        json_secret = json.loads(secret)
+        connection_string = json_secret['CONNECTION_STRING']
         return connection_string
