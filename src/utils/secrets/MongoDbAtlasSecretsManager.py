a42487a4f479acc3458a6600d8ed4a5a1cf0288f
samdouble <samleafs@protonmail.com>
2023-11-09 20:02:57 -0500

    Change datajitsu to binoqlars

--- src/utils/secrets/MongoDbAtlasSecretsManager.py
+++ src/utils/secrets/MongoDbAtlasSecretsManager.py
@@ -4,7 +4,7 @@ from src.utils.secrets.SecretsManager import SecretsManager
 class MongoDbAtlasSecretsManager(SecretsManager):
     @classmethod
     def get_connection_string(cls):
-        secret_name = 'datajitsu_6c98210f-ff3b-4d4a-8935-698fe2ad0287'
+        secret_name = 'binoqlars_6c98210f-ff3b-4d4a-8935-698fe2ad0287'
         secret = cls.get_secret(secret_name)
         json_secret = json.loads(secret)
         connection_string = json_secret['CONNECTION_STRING']
