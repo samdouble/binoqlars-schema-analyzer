2cacdf169f8e024c35481d369885a130b3b1381e
samdouble <samleafs@protonmail.com>
2023-11-13 21:11:23 -0500

    Fix return

--- src/controllers/checks/check.py
+++ src/controllers/checks/check.py
@@ -26,7 +26,7 @@ class Check:
             },
         })
         # Execute actions
-        print(actions, map(lambda a: a.get_id(), actions), map(lambda a: type(a), actions))
+        print(actions, list(map(lambda a: a.get_id(), actions)), list(map(lambda a: type(a), actions)))
         for json_action in self.actions:
             action_id = json_action["id"]
             action = next(filter(lambda a: a.get_id() == action_id, actions))
