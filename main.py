164f984d626d59ced76c433fd11d03839a24b490
samdouble <samleafs@protonmail.com>
2023-11-13 21:07:42 -0500

    Fix return

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ from src.controllers.checks.checks_controller import ChecksController
 
 def handler(_event, _context):
     check = ChecksController.get_one()
-    print("Check", check)
 
     return check.execute()
