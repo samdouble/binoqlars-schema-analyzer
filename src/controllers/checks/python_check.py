bca5e5e4e7119f15d92873b865e790a5891b8881
samdouble <samleafs@protonmail.com>
2023-11-18 21:01:17 -0500

    exec

--- src/controllers/checks/python_check.py
+++ src/controllers/checks/python_check.py
@@ -13,6 +13,6 @@ class PythonCheck(Check):
     def validate(self):
         print(self.script)
         # TODO Use something else than eval() here. Not safe.
-        x = exec(self.script)
+        x = exec(self.script + "\nreturn_value = handler()")
         print(x)
         return True
